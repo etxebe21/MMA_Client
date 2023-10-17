0d94781daa402d9e44091fd4a222e6469d6f2191
Etxebe <mikelgartzia21@gmail.com>
2023-10-17 12:25:30 +0200

    [NEW] creado modal para inciar aplicacion en login y caudo compruebe que estoy logeado se cierre y vea las pantallas de la aplicacion

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React , {useState} from 'react';
+import { View, Modal, StyleSheet } from 'react-native';
 import { NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -13,15 +14,36 @@ import Login from './screens/Login';
 
 const App =  () => {
   const Tab = createMaterialTopTabNavigator();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoginModalVisible, setLoginModalVisible] = useState(true);
+
+  const handleLogin = () => {
+    // Aquí debes realizar la lógica de autenticación
+    // Si la autenticación es exitosa, establece isAuthenticated en true y cierra el modal
+    setIsAuthenticated(true);
+    setLoginModalVisible(false);
+  };
 
   return(
-    <>
-
-    <Splash/>
-
-    <Login/>
     
-      {/* <Header/>  
+    //<Splash/>
+<View style = {{ flex: 1}}>
+    <Modal
+        animationType="slide"
+        transparent={false}
+        visible={isLoginModalVisible}
+        onRequestClose={() => {
+          setLoginModalVisible(false);
+        }}
+      >
+       <View style={styles.modalContainer}>
+          <Login onLogin={handleLogin} />
+        </View>
+      </Modal>
+      {isAuthenticated && (
+   
+        <>
+      <Header/>  
   
     <NavigationContainer>
       <Tab.Navigator
@@ -47,8 +69,21 @@ const App =  () => {
       <Tab.Screen name = "CreatePotions" component={CreatePotions} />
       <Tab.Screen name = "Profile" component={Profile} />
       </Tab.Navigator>
-    </NavigationContainer> */}
+    </NavigationContainer>
     </>
+      )}
+      </View>
   );
+  
 };
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#C8A2C8',
+  },
+});
+
 export default App
