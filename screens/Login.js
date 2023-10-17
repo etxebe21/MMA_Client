6465eb1f30a70f70aa534a0359e6a7dda74a6676
Etxebe <mikelgartzia21@gmail.com>
2023-10-17 10:53:02 +0200

    [NEW] creado spinner que bloquea pantalla al pulsar boton de login

--- screens/Login.js
+++ screens/Login.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
+import { ActivityIndicator } from "react-native";
 
 const View = styled.View`
     flex: 1;
     background: #C8A2C8;
-`
-const Text = styled.Text `
+`;
+
+const Text = styled.Text`
     bottom: -100px;
     color: #4c2882;
     font-size: 30px;
     font-weight: bold;
     letter-spacing: -0.3px;
-    align-self: center; 
-`
+    align-self: center;
+`;
+
 const StyledButton = styled.TouchableOpacity`
     background-color: #4c2882;
     padding: 10px 20px;
@@ -27,15 +30,28 @@ const ButtonText = styled.Text`
     color: white;
     font-size: 20px;
     text-align: center;
-`
-
+`;
 
 const Login = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleGoogleSignIn = () => {
+        setIsLoading(true);
+        //onGoogleButtonPress()
+            // .then(() => {
+            //     console.log('Signed in with Google!');
+            //     // Realiza acciones después de iniciar sesión si es necesario
+            // })
+            // .finally(() => {
+            //     setIsLoading(false);
+            // });
+    };
+
     return (
         <View>
             <Text>LOGIN</Text>
-            <StyledButton onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}>
-                <ButtonText>Google Sign-In</ButtonText>
+            <StyledButton onPress={handleGoogleSignIn} disabled={isLoading}>
+                {isLoading ? <ActivityIndicator color="white" /> : <ButtonText>Google Sign-In</ButtonText>}
             </StyledButton>
         </View>
     );
