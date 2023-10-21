c0f7c0a072fecfc52c90b3c3192766352027ba09
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-10-21 14:11:23 +0200

    [NEW] Creacion de fondo epico en CreatePotions screen - COMPLETADA

--- screens/CreatePotions.js
+++ screens/CreatePotions.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styled from "styled-components/native";
+import { ImageBackground, StyleSheet } from 'react-native'
+
+const bgImage = '../assets/wallpaper_potionCreation3.png';
 
 const View = styled.View`
     flex: 1;
     background: #C8A2C8;
+    heigth: 100%;
 `
 
 const Text = styled.Text `
-    bottom: -100px;
+    bottom: 350;
     color: #4c2882;
     font-size: 25px;
     font-weight: bold;
@@ -16,18 +20,31 @@ const Text = styled.Text `
     
 `
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: null,
+        width: null
+    }
+})
+
+
 const CreatePotions = () => {
 
     return(
         
-        
-        <View>
-                
-             <Text> CREATE POTIONS </Text>
-                
+        < View>
+            <ImageBackground source={require(bgImage)} style={styles.container}>
+                    
+                    <Text> CREATE POTIONS </Text>
+
+            </ImageBackground>
         </View>
-       
     )
 }
+
+
 
 export default CreatePotions;
