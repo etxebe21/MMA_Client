9059d698fb3f69525534b50f2513c9ec4b4b1aef
Etxebe <mikelgartzia21@gmail.com>
2023-11-04 20:38:53 +0100

    [NEW] creado modal que aparece al pulsar en un usuario y que se pueda pulsar en ellos

--- screens/Mortimer.js
+++ screens/Mortimer.js
@@ -1,11 +1,13 @@
 import React , {useState, useEffect} from "react";
 import styled from "styled-components/native";
-import { Modal , StyleSheet,Image} from "react-native";
+import { Modal , StyleSheet, TouchableOpacity} from "react-native";
 import axios from "axios";
 import { ScrollView } from "react-native";
 
 const Mortimer = () => {
   const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     async function getUsersFromDatabase() {
@@ -26,34 +28,53 @@ const Mortimer = () => {
 
   const acolitos = users.filter(user => user.role === "ACÓLITO");
  
+  const handleUserPress = (user) => {
+    setSelectedUser(user);
+    setModalVisible(true);
+  };
+
   return (
     
     <View style={styles.container}>
-    <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: 50 }} > 
-      <Text>ACÓLITOS</Text>
-      {acolitos.map((data) => (
-        <UserContainer key={data.picture}>
-          <AvatarContainer>
-            <Avatar source={{ uri: data.picture }} />
-            <StatusIndicator isInsideTower={data.insideTower} />
-          </AvatarContainer>
-          <NameText>{data.username}</NameText>
-        </UserContainer>
-      ))}
+      <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: 50 }}>
+        <Text>ACÓLITOS</Text>
+        {acolitos.map((data) => (
+          <TouchableOpacity key={data.picture} onPress={() => handleUserPress(data)}>
+            <UserContainer>
+              <AvatarContainer>
+                <Avatar source={{ uri: data.picture }} />
+                <StatusIndicator isInsideTower={data.insideTower} />
+              </AvatarContainer>
+              <NameText>{data.username}</NameText>
+            </UserContainer>
+          </TouchableOpacity>
+        ))}
       </ScrollView>
+      {selectedUser && (
+        <Modal visible={modalVisible}>
+          <ModalContent>
+            <Text>{selectedUser.username}</Text>
+            <Text>Level: {selectedUser.level}</Text>
+            <Text>Hitpoints: {selectedUser.hitPoints}</Text>
+            {/* Agrega más propiedades según sea necesario */}
+            <CloseButton onPress={() => setModalVisible(false)}>
+              <ButtonText>Close</ButtonText>
+            </CloseButton>
+          </ModalContent>
+        </Modal>
+      )}
     </View>
-    
   );
 };
 
 const styles = StyleSheet.create({
-    modalContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#C8A2C8',
-    },
-  });
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#C8A2C8',
+  },
+});
   
 const View = styled.View`
   flex: 1;
@@ -81,8 +102,8 @@ const Avatar = styled.Image`
   margin-left: 10px;
   padding:1px;
   border-radius: 35px;
-  border: #4c2882;
-  borderWidth: 3px;
+  border-color: #4c2882;
+  border-width: 3px;
 `
 const AvatarContainer = styled.View`
   flex-direction: row;
@@ -104,6 +125,28 @@ const StatusIndicator = styled.View`
   bottom: -20px;
   background-color: ${(props) => (props.isInsideTower ? '#10D24B' : 'red')};
   border: #4c2882;
+`
+const ModalContent = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #d9a9c9;
+`
+const CloseButton = styled.TouchableOpacity`
+    background-color: #4c2882;
+    padding: 10px 20px;
+    bottom: -50px;
+    width: 50%;
+    height: 55px;
+    border-radius: 60px;
+    align-self: center;
+`
+const ButtonText = styled.Text `
+  color: #FFFFFF;
+  font-size: 25px;
+  font-weight: bold;
+  letter-spacing: -0.3px;
+  align-self: center;  
 `
 
 export default Mortimer;
