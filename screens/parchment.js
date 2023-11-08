171974a376b3b3cfc3025811c8f304abe1993ff1
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-11-08 19:34:52 +0100

    [NEW] Creado boton de limpieza de pergamino + pantalla de pergamino sucio

--- screens/parchment.js
+++ screens/parchment.js
@@ -11,41 +11,46 @@ const Parchment = () => {
     return(
     
     <View>
-     <ImageBackground source={require(bgImage)} style={styles.container}> 
-
+        <ImageBackground source={require(bgImage)} style={styles.container}> 
+            <ClearParchmentButton onPress={() => { console.log("Pulsado Boton Pergamino")}}>
+                <ClearParchmentText>Create Potion</ClearParchmentText>
+            </ClearParchmentButton>
         </ImageBackground>
     </View>
     )
 }
+
+
+const ClearParchmentText = styled.Text`
+  fontSize: 25px;
+  color: #4c2882; 
+  align-self: center;
+  top: 5px;
+`
+
+const ClearParchmentButton = styled.TouchableOpacity`
+  top: -150px;
+  background: #CCCCCC;
+  width: 180px;
+  height: 50px;
+  align-self: center;
+  border-radius: 30px;
+  border: #4c2882; 
+`
 
 const View = styled.View`
     flex: 1;
     background: #C8A2C8;
 `
 
-const Text = styled.Text `
-    bottom: -8px;
-    color: #4c2882;
-    font-size: 24px;
-    font-weight: bold;
-    letter-spacing: -0.3px;
-    align-self: center;  
-`
-const TorreonView = styled.View`
-    bottom: -50px;
-    width: 350px;
-    height: 420px;
-    align-self: center;
-    background: #4c2882;
-    border-radius: 30px; 
-` 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         height: null,
-        width: null
+        width: null,
+        backgroundColor: '#C8A2C8'
     }
 })
 
