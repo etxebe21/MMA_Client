ccbd71660aeabc904d573f527f05048440bc998c
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-11-09 20:20:50 +0100

    [NEW] Implementacion de boton de confirmacion para entrar torreon + ocultar dicho boton tras pulsarlo + modificacion a la hora de entrar al pergamino

--- screens/Torreon.js
+++ screens/Torreon.js
@@ -10,20 +10,52 @@ const Torreon = () => {
     
     // Aqui coge el dato de si esta o no en el torreon (Lo he puesto a true todo el rato)
     const [isInTower, setIsInTower] = useState(); 
+    const [isEnteringTower, setIsEnteringTower] = useState(false);
+    const [botonTowerVisible, setBotonTowerVisible] = useState(true);
+
+    const ocultarBoton = () => { setBotonTowerVisible(false) };
+    
+    const enteringTowerButton = () => { setIsEnteringTower(true); ocultarBoton(); };
+
 
     useEffect(() => { 
-        setIsInTower(true) }, []); 
+        setIsInTower(true) 
+    }, []); // Lo deberia de poner segun el get del usuario que hagamos tras scaneo
 
 
     return(
     
     <View>
-     <ImageBackground source={require(bgImage)} style={styles.container}>    
-        <Parchment towerBoolean={isInTower}/> 
+     <ImageBackground source={require(bgImage)} style={styles.container}> 
+        {isInTower && (   
+            <>
+            {botonTowerVisible && <EnterTowerButton onPress={() => { enteringTowerButton()} }>
+                    <EnterTowerButtonText> Enter Tower </EnterTowerButtonText>
+                </EnterTowerButton> }
+                <Parchment towerBoolean={isInTower && isEnteringTower}/> 
+            </>
+        )}
         </ImageBackground>
     </View>
     )
 }
+
+const EnterTowerButtonText = styled.Text`
+  fontSize: 25px;
+  color: #4c2882; 
+  align-self: center;
+  top: 5px;
+`
+
+const EnterTowerButton = styled.TouchableOpacity`
+  top: 220px;
+  background: #CCCCCC;
+  width: 180px;
+  height: 50px;
+  align-self: center;
+  border-radius: 30px;
+  border: #4c2882;
+`
 
 const View = styled.View`
     flex: 1;
