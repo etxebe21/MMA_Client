a3316807fb9465b54ffce37398f567b2a9fcbc08
Etxebe <mikelgartzia21@gmail.com>
2023-11-15 08:58:15 +0100

    [NEW] dados permisos a a la aplicacion para geolocalización en androidManifest

--- screens/Geolocation.js
+++ screens/Geolocation.js
@@ -1,37 +1,79 @@
-import React from 'react';
-import { StyleSheet} from "react-native";
-import styled from "styled-components/native";
-import MapView from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, PermissionsAndroid } from 'react-native';
+import styled from 'styled-components/native';
+import MapView, { Marker } from 'react-native-maps';
 
 const Geolocation = () => {
+  const [region, setRegion] = useState(null);
 
+  useEffect(() => {
+    // Solicitar permiso para acceder a la ubicación del usuario
+    const requestLocationPermission = async () => {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'App Location Permission',
+            message: 'App needs access to your location.',
+          }
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          navigator.geolocation.getCurrentPosition(
+            position => {
+              const { latitude, longitude } = position.coords;
+              setRegion({
+                latitude,
+                longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              });
+            },
+            error => console.log(error),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+          );
+        } else {
+          console.log('Location permission denied');
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    };
 
-    return(
-        <View>
-            <Text>ARTEFACTS  GEOLOCATION</Text>
-        </View>
-    )
-}
+    requestLocationPermission();
+  }, []);
+
+  return (
+    <Container>
+      {region ? (
+        <MapView
+          style={styles.map}
+          region={region}
+          showsUserLocation
+          followsUserLocation
+        >
+          <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+        </MapView>
+      ) : (
+        <LoadingText>Loading...</LoadingText>
+      )}
+    </Container>
+  );
+};
 
 const styles = StyleSheet.create({
-    modalContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
-  
-const View = styled.View`
-    flex: 1;
-    background: #C8A2C8;
-`
-const Text = styled.Text `
-    bottom: -50px;
-    color: #4c2882;
-    font-size: 25px;
-    font-weight: bold;
-    letter-spacing: -0.3px;
-    align-self: center;  
-`
+  map: {
+    flex: 1,
+  },
+});
+
+const Container = styled.View`
+  flex: 1;
+`;
+
+const LoadingText = styled.Text`
+  flex: 1;
+  align-self: center;
+  margin-top: 50%;
+`;
 
 export default Geolocation;
