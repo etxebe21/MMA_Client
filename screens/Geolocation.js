9a851a34496234dfa5103dfb2d381102a9c95abc
soraxkairi <=>
2023-11-21 12:38:07 +0100

    [NEW] Añadido boton para recoger artefacto y aparece cuando estas cerca de un artefacto

--- screens/Geolocation.js
+++ screens/Geolocation.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, PermissionsAndroid, Button, Alert,ImageBackground, Image, View } from 'react-native';
+import { StyleSheet, PermissionsAndroid, Button, Alert, ImageBackground, Image, View } from 'react-native';
 import styled from 'styled-components/native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
@@ -8,10 +8,11 @@ import axios from 'axios';
 const GeolocationUser = () => {
   const [userLocation, setUserLocation] = useState(null);
   const [artifacts, setArtifacts] = useState([]);
+  const [showButton, setShowButton] = useState(false);
 
   const img = require("../assets/geofondo.png")
   const userImage = require("../assets/newPotion.png")
-  const markerLocation = { latitude: 43.25320406434306, longitude: -2.019308098147169};
+  const markerLocation = { latitude: 43.25320406434306, longitude: -2.019308098147169 };
   useEffect(() => {
     const requestLocationPermission = async () => {
       try {
@@ -20,18 +21,17 @@ const GeolocationUser = () => {
         );
 
         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          navigator.geolocation.watchPosition(
-            (position) => {
-              const { latitude, longitude } = position.coords;
-              setUserLocation({ latitude, longitude });
-
-              // Comprobar si la ubicación está dentro del área de un marcador
-              checkIfUserNearMarker({ latitude, longitude });
+          Geolocation.watchPosition(
+            position => {
+              setUserLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+              });
             },
             (error) => {
               console.error('Error al obtener la ubicación:', error);
             },
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 }
+            { enableHighAccuracy: true, timeout: 5000, maximumAge: 1000, distanceFilter: 1 }
           );
         } else {
           console.log('Permiso de ubicación denegado');
@@ -45,53 +45,62 @@ const GeolocationUser = () => {
     requestLocationPermission();
   }, []);
 
-  const checkIfUserNearMarker = ({ latitude, longitude }) => {
+  useEffect(() => {
+    if (userLocation) {
+
+      console.log(userLocation.latitude);
+      checkIfUserNearMarker(userLocation.latitude, userLocation.longitude);
+    }
+  }, [userLocation]);
+
+  const checkIfUserNearMarker = (latitude, longitude) => {
+    console.log(latitude, longitude);
     artifacts.forEach((artifact) => {
       const distance = calculateDistance(latitude, longitude, artifact.latitude, artifact.longitude);
-
-      if (distance < 0.01) {
+      console.log(distance);
+      if (distance < 3500) {
         console.log('Estás cerca del marcador:', artifact.name);
-        // updateFoundedArtifact(artifact);
+
+        setShowButton(true);
       }
+      else setShowButton(false);
     });
   };
 
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
     const R = 6371e3;
-    const x = (lon2-lon1) * Math.cos((lat1+lat2)/2);
-    const y = (lat2-lat1);
-    const d = Math.sqrt(x*x + y*y) * R;
+    const x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
+    const y = (lat2 - lat1);
+    const d = Math.sqrt(x * x + y * y) * R;
     return d;
   };
-
-  
 
   const getArtifactsFromDataBase = async () => {
     try {
       const url = 'https://mmaproject-app.fly.dev/api/artifacts';
       const response = await axios.get(url);
-      const artifacts= response.data.data;
+      const artifacts = response.data.data;
       setArtifacts(artifacts);
-      
+
       console.log('Artefactos:', artifacts);
     } catch (error) {
       console.error('Error al obtener artefactos:', error);
     }
-  };   
-  
+  };
+
   const updateFoundedArtifact = async (artifact) => {
     try {
       const foundedArtifact = { found: true }; // Nuevo estado del artefacto
-  
+
       console.log('ID del artefacto encontrado:', artifact._id);
-  
+
       // Realiza una solicitud PATCH al servidor para actualizar los datos del artefacto
       const response = await axios.patch(`https://mmaproject-app.fly.dev/api/artifacts/updateArtifact/${artifact._id}`, foundedArtifact);
       const updatedArtifact = response.data;
       console.log('Datos del artefacto actualizados:', updatedArtifact);
-  
+
       getArtifactsFromDatabase();
-  
+
       // Muestra un mensaje de confirmación
       Alert.alert(
         "Artefacto Encontrado",
@@ -109,14 +118,10 @@ const GeolocationUser = () => {
       console.error('Error al actualizar los datos del artefacto:', error);
     }
   };
-  
 
-  
-    return (
-      <Container>
-       
-
-       <MapView
+  return (
+    <Container>
+      <MapView
         provider={PROVIDER_GOOGLE}
         style={{ flex: 1 }}
         region={{
@@ -125,147 +130,127 @@ const GeolocationUser = () => {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-        showsUserLocation = {true}
+        showsUserLocation={true}
       >
 
-{artifacts &&
-        artifacts
-          .filter(artifact => !artifact.found) // Filtrar solo artefactos no encontrados
-          .map((artifact, index) => (
-            <Marker
-              key={index}
-              coordinate={{
-                latitude: artifact.latitude,
-                longitude: artifact.longitude,
-              }}
-              title={artifact.name}
-              description={artifact.description || ''}
-            >
-              {/* Contenedor del marcador con imagen */}
-              <View style={styles.markerImageContainer}>
-                <Image
-                  source={{ uri: artifact.image }}
-                  style={styles.roundedMarkerImage}
-                />
-              </View>
-            </Marker>
-          ))}
-    </MapView>
-
-        {/* {userLocation &&
-          userLocation.latitude === artifact1.latitude &&
-          userLocation.longitude === artifact1.longitude && (
-            <Button
-            title="Estás en la misma ubicación que el marcador"
-            onPress={isUserNearMarker}
-            />
-            )} */}
-            
-        <BackgroundImage source={img}>
-          <Title>Artifacts</Title>
-
-          <View style={styles.artifactsContainer}>
-            {artifacts.slice(0, 4).map((artifact, index) => (
-              <View key={index} style={styles.artifactContainer}>
-                <Image
-                  source={{ uri: artifact.image }}
-                  style={[styles.roundedArtifactImage,{ opacity: artifact.found ? 1 : 0.4 }
-                  ]}/>
-              </View>
+        {artifacts &&
+          artifacts
+            .filter(artifact => !artifact.found) // Filtrar solo artefactos no encontrados
+            .map((artifact, index) => (
+              <Marker
+                key={index}
+                coordinate={{
+                  latitude: artifact.latitude,
+                  longitude: artifact.longitude,
+                }}
+                title={artifact.name}
+                description={artifact.description || ''}
+              >
+                {/* Contenedor del marcador con imagen */}
+                <View style={styles.markerImageContainer}>
+                  <Image
+                    source={{ uri: artifact.image }}
+                    style={styles.roundedMarkerImage}
+                  />
+                </View>
+              </Marker>
             ))}
-          </View>
+      </MapView>
+      <BackgroundImage source={img}>
+        {showButton && (
 
-        </BackgroundImage>
-      </Container>
-    );
-  };
+          <Buttons onPress={() => updateFoundedArtifact()}>
+            <ButtonsText>RECOGER</ButtonsText>
+          </Buttons>
+        )}
 
-  const styles = StyleSheet.create({
-    markerImageContainer: {
-      width: 40,
-      height: 40,
-      borderRadius: 20, 
-      overflow: 'hidden', 
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    roundedMarkerImage: {
-      width: '100%',
-      height: '100%',
-      borderRadius: 20, 
-    },
-    artifactsContainer: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-between',
-      paddingHorizontal: 30,
-      paddingVertical: 10,
-    },
-    artifactContainer: {
-      width: 60,
-      height: 60,
-      borderRadius: 50,
-      overflow: 'hidden',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: 30,
-    },
-    roundedArtifactImage: {
-      width: '100%',
-      height: '100%',
-      borderRadius: 50,
-    },
-  });
+        <Title>Artifacts</Title>
+        <View style={styles.artifactsContainer}>
+          {artifacts.slice(0, 4).map((artifact, index) => (
+            <View key={index} style={styles.artifactContainer}>
+              <Image
+                source={{ uri: artifact.image }}
+                style={[styles.roundedArtifactImage, { opacity: artifact.found ? 1 : 0.4 }
+                ]} />
+            </View>
+          ))}
+        </View>
+
+      </BackgroundImage>
+    </Container>
+  );
+};
+const styles = StyleSheet.create({
+  markerImageContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    overflow: 'hidden',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  roundedMarkerImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 20,
+  },
+  artifactsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    bottom: 20
+  },
+  artifactContainer: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    overflow: 'hidden',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  roundedArtifactImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 50,
+  },
+});
 
 const BackgroundImage = styled(ImageBackground)`
   flex: 1;
   resizeMode: cover;
   justify-content: center;
   opacity:0.8
-`
+  `
+const Buttons = styled.TouchableOpacity`
+  background: #A3A2A2;
+  opacity: 0.95;
+  width: 180px;
+  height: 65px;
+  align-self: center;
+  border-radius: 30px;
+  border: #0B0B0B;
+  bottom:25px;
+  background-color:#ffffff
+  `
+const ButtonsText = styled.Text`
+  fontSize: 28px;
+  font-family: 'Tealand';
+  color: #4c2882; 
+  align-self: center;
+  top:10px;
+  `
 const Container = styled.View`
   flex: 1;
-`
-const Artifact1 = styled.View`
-  border:5px;
-  background-color: #CBCACA; 
-  padding: 40px;
-  align-self: center;
-  margin-right:170px;
-  margin-top:10px;
-  border-radius:50px;
-`
-const Artifact2 = styled.View`
-  border:5px;
-  background-color: #CBCACA; 
-  padding: 40px;
-  align-self: center;
-  margin-left:200px;
-  margin-top:-90px;
-  border-radius:50px;
-`
-const Artifact3 = styled.View`
-  border:5px;
-  background-color: #CBCACA; 
-  padding: 40px;
-  align-self: center;
-  margin-right:170px;
-  margin-top:30px;
-  border-radius:50px;
-`
-const Artifact4 = styled.View`
-  border:5px;
-  
-  padding: 40px;
-  align-self: center;
-  margin-left:200px;
-  margin-top:-90px;
-  border-radius:50px;
-`
+  `
+
 const Title = styled.Text`
 font-size: 40px; 
 align-self:center;
 color:#000000;
+font-family: 'Tealand';
+bottom:20px;
 `
 export default GeolocationUser;
-
