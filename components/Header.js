10b939a8affb380f3f4592d244f861f8e9f8f39c
soraxkairi <asier.nogueira@ikasle.aeg.eus>
2023-11-24 00:13:29 +0100

    [Update] modifado estilos de header

--- components/Header.js
+++ components/Header.js
@@ -1,46 +1,46 @@
 import React from 'react';
 import styled from 'styled-components/native';
-
+import { Image } from 'react-native';
 
 const Container = styled.View`
-    width: 100%;
-    height: 85px;
-    padding: 0 11px;
-    align-items: center;
-    flex-direction: row;
-    justify-content: space-between; 
-    background: #C8A2C8;
-    `
+  width: 100%;
+  height: 85px;
+  padding: 0 11px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  background: #2e1b3c;
+  elevation: 5;
+  opacity:1;
+`;
 
+const Text = styled.Text`
+  margin-top: 5px;
+  color: #bf55ec; 
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: -0.3px;
+  font-family: 'Tealand';
+`;
 
-const Text = styled.Text `
-    margin-top: 40px;
-    color: #4c2882;
-    font-size: 30px;
-    font-weight: bold;
-    letter-spacing: -0.3px;
-    font-family: 'Tealand';
-`
 const Row = styled.View`
-    flex-direction: row;
-`
+  flex-direction: row;
+`;
 
-const Button = styled.TouchableOpacity`
-    width: 42px;
-    height: 42px;
-    border-radius: 21px;
-    margin-left: 16px;
-    background: #C8A2C8;
-    align-items: center;
-    justify-content: center;
-`
+
 
 const Header = () => {
-    return(
-        <Container>
-            <Text> LA HERMANDAD </Text> 
-        </Container>
-    )
-}
-export default Header;
+  return (
+    <Container>
+      <Row>
+          <Image source={require('../assets/chaos2.png')} style={{ width: 60, height: 60, borderRadius:40 }} />
+      </Row>
+      <Text>LA HERMANDAD</Text>
+      <Row>
+          <Image source={require('../assets/chaos1.png')} style={{ width: 60, height: 60, borderRadius:40 }} />
+      </Row>
+    </Container>
+  );
+};
 
+export default Header;
