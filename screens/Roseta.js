f2de578096ecc488e1a06453cc8a186fa333e431
Etxebe <mikelgartzia21@gmail.com>
2023-11-24 09:33:57 +0100

    [NEW] se muestran las imaganes de los 4 artefactos en el roseton

--- screens/Roseta.js
+++ screens/Roseta.js
@@ -1,52 +1,61 @@
-import React from "react";
-import styled from "styled-components/native";
-import { ImageBackground, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components/native';
+import { ImageBackground, StyleSheet, Image } from 'react-native';
+import axios from 'axios';
 
 const bgImage = require('../assets/roseta.png');
 
+const Roseta = () => {
+  const [artifactImages, setArtifactImages] = useState([]);
 
-const Roseta = () => { 
+  useEffect(() => {
+    getArtifactsFromDataBase();
+  }, []);
 
-    return(
-        <View>
-            <BackgroundImage source= {bgImage}></BackgroundImage>
-        </View>
-    )
-
-
-}
-
-const View = styled.View`
-    flex: 1;
-    background: #C8A2C8;
-    heigth: '100%';
-    top:-13px;
-`
-
-const Text = styled.Text `
-    bottom: -13px;
-    color: #C8A2C8;
-    font-size: 25px;
-    font-weight: bold;
-    letter-spacing: -0.3px;
-    align-self: center; 
-    
-`
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: null,
-        width: null
+  const getArtifactsFromDataBase = async () => {
+    try {
+      const url = 'https://mmaproject-app.fly.dev/api/artifacts';
+      const response = await axios.get(url);
+      const artifacts = response.data.data;
+      setArtifactImages(artifacts.map((artifact) => artifact.image));
+    } catch (error) {
+      console.error('Error al obtener imágenes de artefactos:', error);
     }
-})
+  };
+
+  // Estilos individuales para cada imagen de artefacto
+  const artifactStyles = [
+    { width: 80, height: 80, borderRadius: 40, borderWidth: 2, borderColor: 'red', marginRight: 10, top: -10},
+    { width: 80, height: 80, borderRadius: 40, borderWidth: 2, borderColor: 'lightblue', marginRight: 10, top: 210},
+    { width: 80, height: 80, borderRadius: 40, borderWidth: 2, borderColor: 'green', bottom: 20, marginRight: 300},
+    { width: 80, height: 80, borderRadius: 40, borderWidth: 2, borderColor: 'blue', marginLeft: 290, top: -100},
+  ];
+
+  return (
+    <BackgroundContainer>
+      <BackgroundImage source={bgImage}>
+        
+          {artifactImages.length === 4 &&
+            artifactImages.map((image, index) => (
+              <ArtifactImage key={index} source={{ uri: image }} style={artifactStyles[index]} />
+            ))}
+       
+      </BackgroundImage>
+    </BackgroundContainer>
+  );
+};
+
+const BackgroundContainer = styled.View`
+  flex: 1;
+  background-color: #c8a2c8;
+`;
 
 const BackgroundImage = styled(ImageBackground)`
   flex: 1;
-  resizeMode: cover;
   justify-content: center;
-  opacity:0.8
-  `
+  align-items: center;
+`;
+
+const ArtifactImage = styled(Image)``;
+
 export default Roseta;
