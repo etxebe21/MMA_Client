b64d1a255d6941b37521d9e5f03bb5945f78a993
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-11-28 10:34:20 +0100

    [UPDATE] Nuevas imagenes de fondo y marco + cambios visuales en todo el perfil exceptuando estadisticas - Comienzo de cambio de estadisticas

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
 import axios from "axios";
-import { ScrollView } from "react-native";
+import { ImageBackground, ScrollView, StyleSheet } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { StyledProgressBar } from '../components/ProgressBar';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -35,27 +35,24 @@ const Profile = () => {
   return (
 
     <View >
+      <ImageBackground source={require("../assets/wallpaperProfile.png")} style={styles.imageBackground}>
       {user && (
-          <Content>
+        <Content>
           <AvatarBox>
-            <AdditionalImageBackground
-              source={require("../assets/bosque.jpg")}
-            >
-            <Marco
-            source={require("../assets/marco.png")}
-            >
+              
               <DetailAvatar source={{ uri: user.picture }} style={{ width: 90, height: 90, borderRadius: 45 }} />
+              <MarcoFoto source={require("../assets/marcoEpico.png")}/>
+              
               <UserLevelMarco>
-              <UserTextLevel> {user.level}</UserTextLevel>
+                <UserTextLevel> {user.level}</UserTextLevel>
               </UserLevelMarco>
-            </Marco>
+              
               <UserText>{user.username}</UserText>
-            </AdditionalImageBackground>
           </AvatarBox>
 
 
           <Statsbackground
-          source={require("../assets/city.jpg")}
+          
           >
           <ProgressBarRow>       
             <ProgressBarColumn>
@@ -84,21 +81,37 @@ const Profile = () => {
         </Content>
 
       )}
+      </ImageBackground>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  imageBackground: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+  
+});
+
 const Content = styled.View`
-  padding: 20px;
+  heigth: 100%;
+  width: 100%;
+
 `;
 
 const View = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: #d9a9c9;
-    height: auto; 
-  `
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%; 
+  width: 100%;
+`
 const Text = styled.Text`
     bottom: -15px;
     color: #4c2882;
@@ -108,66 +121,82 @@ const Text = styled.Text`
     align-self: center;  
   `
 const AdditionalImageBackground = styled.ImageBackground`
-  width: 360px;
-  height: 165px;
-  overflow: hidden;
-  justify-content: flex-end; 
-`;
+  width: 100%;
+  height: 100%;
+  margin-top: -40%;
+  justify-content: center; 
+`
 
 const Statsbackground = styled.ImageBackground`
-  height:410px;
+  height: 60%;
   overflow: hidden;
   justify-content: flex-end;
   border:3px; 
-`;
+  border-color: pink;
+`
 const UserText = styled.Text`
-    top: -5px;
-    color: #4c2882;
-    font-size: 35px;
-    font-weight: bold;
-    letter-spacing: 0.5px;
-    align-self: center; 
-  `
+  color: white;
+  font-family: Tealand;
+  text-shadow: 3px 3px 4px black;
+  font-size: 20px;
+  margin-top: 1%;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 const UserLevelMarco = styled.View`
-top: -5px;
-color: black;
 align-self: center;
 border:3px;
 border-radius:50px;
-height:50px;
-width:50px;
-left:50px;
-background-color:gray;
+border-color: rgb(124, 44, 245 );
+height: 25%;
+width:  13%;
+left:   13%;
+margin-top: -13%;
+background-color: rgba(255, 255, 255, 0.9);
+display: flex;
+justify-content: center;
+align-items: center;
 `
   const UserTextLevel = styled.Text`
-    color: black;
-    font-size: 25px;
-    font-weight: bold;
-    letter-spacing: 0px;
-    align-self: center;
-    position: absolute;
-    text-align:center;
+  color: black;
+  font-size: 20px;
+  font-weight: bold;
+  margin-left: -10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   `
 const DetailAvatar = styled.Image`
-    width: 110px;
-    height: 110px;
-    border-radius: 45px;
-    border-width: 3px;
-    align-self: center
-    top:27px;
-  `
+  align-self: center;
+`
+
 const AvatarBox = styled.View`
   border: 3px;
-  color: 'black';
-  width:366px;
-  height:170px;
-  `
+  border-color: white;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+`
 
 const Marco = styled.ImageBackground`
-  width:150px;
-  height:150px;
-  align-self: center;
-  top:20px;
+  width:  150px;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const MarcoFoto = styled.Image`
+  width:  150px;
+  height: 150px;
+  border-radius: 100px;
+  margin-top: -32%;
 `
 
 const AdditionalImage = styled.Image`
