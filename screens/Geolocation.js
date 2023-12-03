65692ad6bf7dd5d424b541a3352907fe3dd69092
soraxkairi <asier.nogueira@ikasle.aeg.eus>
2023-12-03 21:11:20 +0100

    Al recoger mostrar toast

--- screens/Geolocation.js
+++ screens/Geolocation.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef, useContext } from 'react';
-import { StyleSheet, PermissionsAndroid, Alert, ImageBackground, Image, View,ActivityIndicator,Animated } from 'react-native';
+import { StyleSheet, PermissionsAndroid, Alert, ImageBackground,ToastAndroid,Image, View,ActivityIndicator,Animated } from 'react-native';
 import styled from 'styled-components/native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
@@ -10,6 +10,7 @@ import Roseta from './Roseta';
 import { Context } from '../context/Context';
 import MapStyle from '../components/MapStyle.json'
 import io, { Socket } from 'socket.io-client';
+
 
 const GeolocationUser = () => {
   //GLOBALES
@@ -48,20 +49,7 @@ const GeolocationUser = () => {
 
     console.log(`GLOBAL ARTEEEEEEEFAAAAACT: ${artifactsGlobalState}`);
 
-    // // Enviar datos al servidor con el evento 'clientEvent'
-    // newSocket.emit('clientEvent', { artifactsGlobalState });
-
-   
-
-    // newSocket.on('new_user', (level) => {
-    //   console.log('Datos recibidos desde el servidor:', level);
-    // });
-
-   
-
-    // newSocket.on('updateArtifacts',() => {
-    //   console.log('Datos recibidos desde el servidor:', );
-    // });
+    
     setSocket(newSocket);
     // Limpia la instancia del socket al desmontar el componente
     return () => {
@@ -249,118 +237,33 @@ useEffect(() => {
     return d;
   };
   
-  // const updateFoundedArtifact = async (artifact) => {
-  //   try {
-    //     const selectedArtifact = { found: !artifact.found , who: userId }; // Invertir el estado de 'found'
-    //     const id = {artifactId: '65571a2c88e0cd6ea51d7b75'};
-  //     //setSelectedArtifact(selectedArtifact);
-  //     console.log("ARTEFACTO SELECCIONADO", selectedArtifact);
-  
-  //     socket.emit('clientEvent', {id, selectedArtifact});
-
-  //     // Escuchar la respuesta del servidor al evento 'responseEvent'
-  //    socket.on('responseEvent', (responseData) => {
-  //     const responseData = await new Promise(resolve => {
-  //       socket.on('responseEvent', (data) => {
-  //           resolve(data);
-  //       });
-  //   });
-  //    setArtifactsGlobalState(responseData);
-  //      if (artifactsGlobalState != null)
-  //      {
-  //        console.log(`Artefactos modificados ${artifactsGlobalState}`);
-  //      }
-
-  //     // // Obtener la imagen del usuario actual
-  //     // const userImage = await getUserImageById(userId);
-  
-  //     // // Actualizar el estado de artefactos localmente con la imagen del usuario que lo recogió
-  //     // const updatedArtifacts = artifactsGlobalState.map(art => {
-  //     //   if (art._id === updatedArtifact._id) {
-  //     //     return { ...updatedArtifact, userImage }; // Actualizar el artefacto recién recolectado con la nueva imagen
-  //     //   } else if (art.found) {
-  //     //     // Mantener la información de la imagen de usuario para los artefactos previamente recolectados
-  //     //     return { ...art, userImage: art.userImage };
-  //     //   }
-  //     //   return art;
-  //     // });
-  
-  //   });
-  //     // Incrementar collectedArtifacts al recoger un artefacto
-  //     setCollectedArtifacts(prevCount => prevCount + 1);
-  //    // // Obtener la imagen del usuario actual
-  //     // const userImage = await getUserImageById(userId);
-  
-  //     // // Actualizar el estado de artefactos localmente con la imagen del usuario que lo recogió
-  //     // const updatedArtifacts = artifactsGlobalState.map(art => {
-  //     //   if (art._id === updatedArtifact._id) {
-  //     //     return { ...updatedArtifact, userImage }; // Actualizar el artefacto recién recolectado con la nueva imagen
-  //     //   } else if (art.found) {
-  //     //     // Mantener la información de la imagen de usuario para los artefactos previamente recolectados
-  //     //     return { ...art, userImage: art.userImage };
-  //     //   }
-  //     //   return art;
-  //     // });
-  //     // Muestra un mensaje de confirmación
-  //     Alert.alert(
-  //       "Artefacto Encontrado",
-  //       "Los datos del artefacto han sido actualizados correctamente.",
-  //       [
-  //         {
-  //           text: "OK",
-  //           onPress: () => {}, // No recargar los artefactos después de presionar "OK" para mantener las imágenes de usuario
-  //         },
-  //       ],
-  //       { cancelable: false }
-  //     );
-  
-  //   } catch (error) {
-  //     console.error('Error al actualizar los datos del artefacto:', error);
-  //   }
-  // };
-
   const updateFoundedArtifact = async (artifact) => {
     try {
-        const selectedArtifact = { found: !artifact.found, who: userId };
-        const id = { artifactId: '65571a2c88e0cd6ea51d7b75' };
-
-        console.log("ARTEFACTO SELECCIONADO", selectedArtifact);
-
-        // Emitir el evento al servidor
-        socket.emit('updateArtifact', { id, selectedArtifact });
-
-        // Esperar la respuesta del servidor al evento 'responseEvent' usando una promesa
-        const responseData = await new Promise(resolve => {
-            socket.on('responseEvent', (data) => {
-                resolve(data);
-            });
-        });
-
-        console.log('Respuesta desde el servidor con artefactos modificados:', responseData);
-
-
-        // Incrementar collectedArtifacts al recoger un artefacto
-        setCollectedArtifacts(prevCount => prevCount + 1);
-
-        // Muestra un mensaje de confirmación
-        Alert.alert(
-            "Artefacto Encontrado",
-            "Los datos del artefacto han sido actualizados correctamente.",
-            [
-                {
-                    text: "OK",
-                    onPress: () => { /* No recargar los artefactos después de presionar "OK" */ },
-                },
-            ],
-            { cancelable: false }
-        );
-
-    } catch (error) {
-        console.error('Error al actualizar los datos del artefacto:', error);
-    }
-};
-
+      const selectedArtifact = { found: !artifact.found, who: userId }; // Invertir el estado de 'found'
+      const id = { artifactId: '65571a2c88e0cd6ea51d7b75' };
   
+      console.log("ARTEFACTO SELECCIONADO", selectedArtifact);
+  
+      // Emitir el evento 'clientEvent' al servidor con los datos actualizados del artefacto
+      socket.emit('updateArtifact', { id, selectedArtifact });
+  
+      // Escuchar la respuesta del servidor al evento 'responseEvent' usando una promesa
+      const responseData = await new Promise((resolve) => {
+        socket.on('responseEvent', (data) => {
+          resolve(data);
+        });
+      });
+  
+      // Incrementar collectedArtifacts al recoger un artefacto
+      setCollectedArtifacts((prevCount) => prevCount + 1);
+  
+      ToastAndroid.showWithGravity('Artefacto recogido',ToastAndroid.SHORT,ToastAndroid.CENTER);
+    } catch (error) {
+      console.error('Error al actualizar los datos del artefacto:', error);
+    }
+  };
+  
+
   
   const getSearchesFromDataBase = async () => {
     try {
@@ -469,6 +372,8 @@ const getAndSendUserLocation = async () => {
     console.error('Error al enviar la ubicación:', error);
   }
 };
+
+
 const getUserLocation = async () => {
   try {
     const location = await new Promise((resolve, reject) => {
@@ -662,10 +567,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   roundedUserImage: {
-    width: 35, // Tamaño deseado de la imagen del usuario
+    width: 35, 
     height:35,
-    borderRadius: 17.5, // Mitad del tamaño deseado para hacerlo redondo
-    borderWidth: 1, // Puedes ajustar el grosor y el color del borde si lo deseas
+    borderRadius: 17.5, 
+    borderWidth: 1, 
     borderColor: '#4c2882',
     top: 27,
     marginLeft: 33
