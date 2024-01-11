285855d714c0889ff450b4b7e1ffb99c29e72014
Etxebe <mikelgartzia21@gmail.com>
2024-01-11 09:25:07 +0100

    [NEW] - Crear refresco de token (si caduca)  enviar a servidor con email

--- keychain.js
+++ keychain.js
@@ -10,7 +10,10 @@ export const setSecureValue = async (key, value) => {
   console.log("GETTING SECURE VALUE")
   const result = await Keychain.getInternetCredentials(key)
   if (result) {
+    console.log(password);
+    console.log(result);
     return result.password
+    
   }
   return false
  }
@@ -42,3 +45,34 @@ export const setSecureValue = async (key, value) => {
     })
   }
  
+  //MANEJAR LA EXPIRACIÓN DEL TOKEN
+  
+  auth().onUserChanged(async (user) => {
+    if (user) {
+      const idTokenResult = await user.getIdTokenResult();
+  
+      // Verificar si el token ha caducado y actualizar si es necesario
+      if (idTokenResult.expirationTime < Date.now()) {
+        const refreshedToken = await user.getIdToken(true);
+        console.log("Nuevo token", refreshedToken);
+        // Obtener el correo electrónico del usuario
+        const userEmail = user.email;
+  
+        // Enviar el correo electrónico y el token actualizado al servidor
+        await sendDataToServer(userEmail, refreshedToken);
+  
+        // Manejar el token actualizado según sea necesario
+      }
+    }
+  });
+  
+  async function sendDataToServer(email, token) {
+    // Realizar la lógica para enviar el correo electrónico y el token al servidor
+    // Puedes usar axios u otra biblioteca para hacer la solicitud al servidor
+    const urlServidor = 'https://mmaproject-app.fly.dev/';
+    const respuestaServidor = await axios.post(urlServidor, { email, token });
+  console.log(respuestaServidor);
+    // Manejar la respuesta del servidor según sea necesario
+  }
+  
+  
