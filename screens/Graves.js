bc0cc45f267448814ac7544919041d3af0e5abad
Etxebe <mikelgartzia21@gmail.com>
2024-01-16 02:30:55 +0100

    [UPDATE] al pulsar una tumba se bloquea para no volver a cogerla y se guarda en el array de inventario que solo va a poder tener 4 componentes dentro

--- screens/Graves.js
+++ screens/Graves.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 
@@ -6,44 +6,52 @@ const Graves = () => {
   const [inventory, setInventory] = useState([]);
 
   const handleSquareClick = (component) => {
-    setInventory((prevInventory) => [...prevInventory, component]);
+    setInventory((prevInventory) => {
+      if (prevInventory.length < 4 && !prevInventory.includes(component)) {
+        return [...prevInventory, component];
+      }
+      return prevInventory;
+    });
   };
 
+  useEffect(() => {
+    console.log(inventory);
+  }, [inventory]);
+
+
   return (
-    <View style={{ flex: 1 }}>
-    {/* Sección superior */}
-    <View style={{ flex: 0.5, flexDirection: 'row' }}>
-      <Square onPress={() => handleSquareClick('Componente 1')}>
-        <Image source={require('../assets/descansoAcolito.png')} style={styles.image} />
-      </Square>
+    <StyledView style={{ flex: 1 }}>
+      {/* Sección superior */}
+      <StyledView style={{ flex: 0.5, flexDirection: 'row' }}>
+        <Square onPress={() => handleSquareClick('Componente 1')} disabled={inventory.includes('Componente 1')}>
+          <Image source={require('../assets/descansoAcolito.png')} style={styles.image} />
+        </Square>
 
-      <Square onPress={() => handleSquareClick('Componente 2')}>
-        <Image source={require('../assets/descansoAcolito.png')} style={styles.image} />
-      </Square>
-    </View>
+        <Square onPress={() => handleSquareClick('Componente 2')} disabled={inventory.includes('Componente 2')}>
+          <Image source={require('../assets/descansoAcolito.png')} style={styles.image} />
+        </Square>
+      </StyledView>
 
-    {/* Sección inferior */}
-    <View style={{ flex: 0.5, flexDirection: 'row' }}>
-      <Square onPress={() => handleSquareClick('Componente 3')}>
-        <Image source={require('../assets/descansoAcolito.png')} style={styles.image} />
-      </Square>
+      {/* Sección inferior */}
+      <StyledView style={{ flex: 0.5, flexDirection: 'row' }}>
+        <Square onPress={() => handleSquareClick('Componente 3')} disabled={inventory.includes('Componente 3')}>
+          <Image source={require('../assets/descansoAcolito.png')} style={styles.image} />
+        </Square>
 
-      <Square onPress={() => handleSquareClick('Componente 4')}>
-        <Image source={require('../assets/descansoAcolito.png')} style={styles.image} />
-      </Square>
-    </View>
-  </View>
-);
+        <Square onPress={() => handleSquareClick('Componente 4')} disabled={inventory.includes('Componente 4')}>
+          <Image source={require('../assets/descansoAcolito.png')} style={styles.image} />
+        </Square>
+      </StyledView>
+    </StyledView>
+  );
 };
 
-
 const Square = styled(TouchableOpacity)`
-  width: 130px;
-  height: 150px;
-  margin: 35px;
+  flex: 1;
+  margin: 10px;
   border: 2px solid black;
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)}; // Ajustar la opacidad si está deshabilitado
 `;
-
 const styles = StyleSheet.create({
   image: {
     width: '100%',
@@ -52,7 +60,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const View = styled.View`
+const StyledView = styled.View`
   background-color: rgba(255,255,255,1)
   flex: 1;
   justify-content: center;
