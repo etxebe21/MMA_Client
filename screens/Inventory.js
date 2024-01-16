386ace7b10f6c6085c2234143a7432fb47dbbdbf
soraxkairi <asier.nogueira@ikasle.aeg.eus>
2024-01-16 23:38:00 +0100

    Modificado valores de imagen y añadido fondo a la pantalla

--- screens/Inventory.js
+++ screens/Inventory.js
@@ -1,46 +1,62 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, ImageBackground } from 'react-native';
 import styled from 'styled-components/native';
 
 const Inventory = () => {
     const [inventory, setInventory] = useState([]);
 
     return (
-        <StyledView>
-            <TextStyled>
-                Entramos en inventario
-            </TextStyled>
-            <Row>
-                <Square disabled={true}>
-                    <Image source={require('../assets/cansado.jpeg')} style={styles.image} />
-                </Square>
-                <Square>
-                    <Image source={require('../assets/cansado.jpeg')} style={styles.image} />
-                </Square>
-            </Row>
-            <Row>
-                <Square>
-                    <Image source={require('../assets/cansado.jpeg')} style={styles.image} />
-                </Square>
-                <Square>
-                    <Image source={require('../assets/cansado.jpeg')} style={styles.image} />
-                </Square>
-            </Row>
-        </StyledView>
+        <ImageBackground
+            source={require('../assets/modal_NonEncriptedParchment.png')}
+            style={{resizeMode: 'cover', justifyContent: 'center'}}
+        >
+            <StyledView>
+                <TextStyled>
+                    Entramos en inventario
+                </TextStyled>
+                <Row>
+                    <Square disabled={true}>
+                        <Image source={require('../assets/cansado.jpeg')} style={styles.image} />
+                    </Square>
+                    <Square>
+                        <Image source={require('../assets/cansado.jpeg')} style={styles.image} />
+                    </Square>
+                </Row>
+                <Siluete source={require('../assets/cansado.jpeg')} />
+                <Row1>
+                    <Square>
+                        <Image source={require('../assets/cansado.jpeg')} style={styles.image} />
+                    </Square>
+                    <Square>
+                        <Image source={require('../assets/cansado.jpeg')} style={styles.image} />
+                    </Square>
+                </Row1>
+            </StyledView>
+        </ImageBackground>
     );
 };
 
 const Row = styled.View`
   flex-direction: row;
   justify-content: space-around;
-  margin-top: 10px;  
+  width:100%;
+  height:25%;
+  padding:2%;
+`;
+
+const Row1 = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  width: 60%;
+  height: 20%;
 `;
 
 const Square = styled.View`
   flex: 1;
-  margin: 10px;
+  margin: 20px;
   border: 2px solid black;
-  opacity: 1};
+  height:70px;
+  width:60px;
 `;
 
 const TextStyled = styled.Text`
@@ -49,12 +65,16 @@ const TextStyled = styled.Text`
 `;
 
 const StyledView = styled.View`
-  background-color: purple;
   flex: 1;
   justify-content: center;
   align-items: center;
   height: 100%;
   width: 100%;
+`;
+
+const Siluete = styled.Image`
+  height:40%;
+  width: 30%;
 `;
 
 const styles = StyleSheet.create({
