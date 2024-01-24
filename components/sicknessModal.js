e4490b44d43c6a7ad7783aba4b4521dec60e3896
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-01-24 09:04:54 +0100

    [NEW] Creados botones para aplicar enfermedades - comienzo de funcionalidad

--- components/sicknessModal.js
+++ components/sicknessModal.js
@@ -2,10 +2,27 @@ import React from "react"
 import { Image, ImageBackground, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
+const RottingPlagueApply = () => {
+  console.log("Rotting Plague Applied");
+}
+
+const EpicWeaknessApply = () => {
+  console.log("Epic Weakness Apply Applied");
+}
+
+const MarrowApocalypseApply = () => {
+  console.log("Marrow Apocalypse Applied");
+}
+
+
 const SicknessModal = ({closeModal}) => {
 
   const Image_background = require('../assets/fondoViejaEscuela.png');
   const Image_ClosedIcon = require('../assets/descansoAcolito.png');
+
+  const Image_RottingPlague     = ''
+  const Image_EpicWeackness     = ''
+  const Image_MArrowApocalypse  = ''
 
   return(
       
@@ -17,6 +34,21 @@ const SicknessModal = ({closeModal}) => {
         <ClosedButton onPress={() => closeModal(false)}>
           <Image source={Image_ClosedIcon} style={styles.disseasClosedIcon} />
         </ClosedButton>
+
+        {/* Botones de Envenenamiento */}
+        <DisseasesContainer>
+          <RottingPlague onPress={() => RottingPlagueApply(false)}>
+            <Image source={Image_ClosedIcon} style={styles.disseasApplyIcons} />
+          </RottingPlague>
+
+          <DisseasTwo onPress={() => EpicWeaknessApply(false)}>
+            <Image source={Image_ClosedIcon} style={styles.disseasApplyIcons} />
+          </DisseasTwo>
+
+          <DisseasThree onPress={() => MarrowApocalypseApply(false)}>
+            <Image source={Image_ClosedIcon} style={styles.disseasApplyIcons} />
+          </DisseasThree>
+        </DisseasesContainer>
         
       </ImageBackground>
     </MainContainer>
@@ -32,8 +64,32 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
     backgroundColor: 'orange'
+  },
+  disseasApplyIcons: {
+    width: 80,
+    height: 80,
+    backgroundColor: 'orange'
   }
 });
+
+const RottingPlague =  styled(TouchableOpacity)`
+  background-color: yellow;
+`
+
+const DisseasTwo =  styled(TouchableOpacity)`
+  background-color: orange;
+`
+
+const DisseasThree =  styled(TouchableOpacity)`
+  background-color: pink;
+`
+
+const DisseasesContainer = styled.View`
+  margin-top: 10%;
+  display: flex;
+  justify-content: row;
+  align-items: center;
+`
 
 const ClosedButton = styled(TouchableOpacity)`
   display: flex;
