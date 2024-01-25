072f180b627b1602aaf97278d0b7d0abdeffe98a
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-01-25 12:21:01 +0100

    [UPDATE] Correccion y cambio de logica de envenenamientos - COMPLETADO

--- components/sicknessModal.js
+++ components/sicknessModal.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect, useContext } from "react"
-import { Image, ImageBackground, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
+import { Image, ImageBackground, StyleSheet, Dimensions, TouchableOpacity, ToastAndroid } from "react-native";
 import styled from "styled-components/native";
 import { Context } from "../context/Context";
+import { socket } from '../socket/socketConnect'
 
 const SicknessModal = ({closeModal, selectedSicknessUser}) => {
 
   // GLOBALES
   const { usersGlobalState,  handleUsersGlobalState }   = useContext(Context);
   
-  const [appliedSicknessUser, setAppliedSicknessUser] = useState([]);
-  // Images
+  // LOCALES
+  // const [appliedSicknessUser, setAppliedSicknessUser] = useState([]);
+  const [acolitos, setAcolitos] = useState([]);
+  
+  // IMAGES
   const Image_background = require('../assets/fondoViejaEscuela.png');
   const Image_ClosedIcon = require('../assets/descansoAcolito.png');
 
@@ -17,16 +21,21 @@ const SicknessModal = ({closeModal, selectedSicknessUser}) => {
   const Image_EpicWeackness     = require('../assets/EpicWeackness.jpeg')
   const Image_MarrowApocalypse  = require('../assets/MarrowApocalypse.jpeg')
 
+  // useEffect(() => {
+  //   console.log("==========================") 
+  //   console.log(appliedSicknessUser.username)
+  //   console.log("Inteligencia: " + appliedSicknessUser.inteligencia)
+  //   console.log("Fuerza: " + appliedSicknessUser.fuerza)
+  //   console.log("Agilidad: " + appliedSicknessUser.agilidad)
+  //   console.log("==========================")
+
+
+  // }, [appliedSicknessUser]);
+
   useEffect(() => {
-    console.log("==========================") 
-    console.log(appliedSicknessUser.username)
-    console.log("Inteligencia: " + appliedSicknessUser.inteligencia)
-    console.log("Fuerza: " + appliedSicknessUser.fuerza)
-    console.log("Agilidad: " + appliedSicknessUser.agilidad)
-    console.log("==========================")
+    setAcolitos(usersGlobalState.filter(user => user.role === "ACÓLITO"));
+  }, [usersGlobalState]);
 
-
-  }, [appliedSicknessUser]);
     
   // ==============================
   //    BOTONES DE ENFERMEDADES
@@ -40,7 +49,7 @@ const SicknessModal = ({closeModal, selectedSicknessUser}) => {
 
       // FALTA EMITIR LA BOOLEANA DE ENFERMEDADES
       
-      setAppliedSicknessUser(appliedSickness("rotting_plague", sickUser))
+      appliedSickness("rotting_plague", sickUser)
     }  
   }
 
@@ -52,7 +61,7 @@ const SicknessModal = ({closeModal, selectedSicknessUser}) => {
 
       // FALTA EMITIR LA BOOLEANA DE ENFERMEDADES 
       
-      setAppliedSicknessUser(appliedSickness("epic_weakness", sickUser))
+      appliedSickness("epic_weakness", sickUser)
     }  
   }
 
@@ -63,7 +72,7 @@ const SicknessModal = ({closeModal, selectedSicknessUser}) => {
       sickUser.marrow_apocalypse = true;
 
       // FALTA EMITIR LA BOOLEANA DE ENFERMEDADES
-      setAppliedSicknessUser(appliedSickness("marrow_apocalypse", sickUser))
+      appliedSickness("marrow_apocalypse", sickUser)
     }
   }
 
@@ -78,25 +87,61 @@ const SicknessModal = ({closeModal, selectedSicknessUser}) => {
       
       // Le quita 75% a la INTELIGENCIA
       case "rotting_plague":
-        sickUser.inteligencia = Math.floor((sickUser.inteligencia * 75) / 100)
+        console.log("Entra en Rotting Plague")
+        const rottingPlague = {
+          id: sickUser._id,
+          inteligencia: Math.max(5, Math.ceil(sickUser.inteligencia * 0.75)),
+          rotting_plague: true,
+        };
+
+        const updatedAcolitos1 = acolitos.map(user =>
+          user._id === rottingPlague.id ? { ...user, rotting_plague: true } : user
+        );
+
+        setAcolitos(updatedAcolitos1);
+        socket.emit('RottingPlague', rottingPlague);
         break;
 
       // Le quita 60% a la FUERZA
       case "epic_weakness":
-        sickUser.fuerza = Math.floor((sickUser.fuerza * 60) / 100)
+        console.log("Entra en Epic Weakness")
+        const epicWeakness = {
+          id: sickUser._id,
+          fuerza: Math.max(5, Math.ceil(sickUser.fuerza * 0.6)),
+          epic_weakness: true,
+        };
+
+        const updatedAcolitos2 = acolitos.map(user =>
+          user._id === epicWeakness.id ? { ...user, epic_weakness: true } : user
+        );
+
+        setAcolitos(updatedAcolitos2);
+        socket.emit('EpicWeakness', epicWeakness);
         break;
 
       // Le quita 30% a la AGILIDAD
       case "marrow_apocalypse":
-        sickUser.agilidad = Math.floor((sickUser.agilidad * 30) / 100)
+        console.log("Entra en Marrow Apocalypse")
+        const marrowApocalypse = {
+          id: sickUser._id,
+          agilidad: Math.max(5, Math.ceil(sickUser.agilidad * 0.3)),
+          marrow_apocalypse: true,
+        };
+
+        const updatedAcolitos3 = acolitos.map(user =>
+          user._id === marrowApocalypse.id ? { ...user, marrow_apocalypse: true } : user
+        );
+
+        setAcolitos(updatedAcolitos3);
+        socket.emit('MarrowApocalypse', marrowApocalypse);
         break;
 
       default:
         console.log("no applied")
         break;
     }
-
-    return sickUser
+    
+    ToastAndroid.showWithGravity('ENFERMEDAD: ' + sickness +  ' INVOCADA', ToastAndroid.SHORT, ToastAndroid.CENTER);
   }
 
   return(
